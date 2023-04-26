652b65694b4a85b2c715241365ce75a2819acc93
David Amon <damon@ivytech.edu>
2023-04-25 21:57:49 -0400

    Directory issue testing

--- monday/monday/settings.py
+++ monday/monday/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'monday' / 'blog' / 'static'
+STATIC_ROOT = BASE_DIRs / 'blog' / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
