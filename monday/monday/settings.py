e5f14aa181ed2cff7f0f2b6170242bc2dadae6ab
David Amon <damon@ivytech.edu>
2023-05-06 01:53:53 -0400

    M07_Tutorial Final Update

--- monday/monday/settings.py
+++ monday/monday/settings.py
@@ -124,3 +124,5 @@ STATIC_ROOT = BASE_DIR / 'blog' / 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
